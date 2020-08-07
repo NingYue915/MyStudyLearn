abd863c84427ebe22f053a5d2f44be084a17eeef
jeffwang520 <jeffwang@chinasystems.com>
2020-08-07 14:06:03 +0800

    Update by Jeff

--- MyWorkSpace/apmagent/deployment/build.sh
+++ MyWorkSpace/apmagent/deployment/build.sh
@@ -1,4 +1,4 @@
-docker image rm -f jeffwang/tomcat-apmagent:1.1
+docker image rm -f jeffwang/tomcat-apmagent:1.2
 rm apmagent.war || true
 cp ../target/apmagent-1.0.0.war apmagent.war
-docker build -t jeffwang/tomcat-apmagent:1.1 .
+docker build -t jeffwang/tomcat-apmagent:1.2 .
